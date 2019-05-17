16410ac47519261c6aa8140d97ad170c13805210
glifosaztecas <45978757+glifosaztecas@users.noreply.github.com>
2019-05-16 20:43:59 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,69 @@
+var seville = "<strong>Seville Oranges / Citrus Aurantium</strong> <br><br> Origin: Southeast Asia <br> Blooming season: Winter and spring <br><br> Harvest when the fruit is bright orange and some of the oranges are beginning to fall off of the tree.";
+
+var navel = "<strong>Navel Oranges / Citrus Sinensis “Navel”</strong> <br><br> Origin: Brazil <br> Blooming season: Winter and spring <br><br>  Harvest in the winter and spring when the fruit is bright orange and some of the oranges are beginning to fall off of the tree.";
+
+var valencia = "<strong>Valencia Oranges / Citrus Sinensis “Valencia”</strong> <br><br> Origin: Santa Ana, California <br> Hybrid of pummelo and mandarin orange <br> Blooming season: Winter and spring <br><br> Harvest in the summer and winter when the fruit is bright orange and some of the oranges are beginning to fall off of the tree.";
+
+var eureka = "<strong>Eureka Lemons / Citrus Limon</strong> <br><br> Origin: China <br> Blooming season: Year-round, peaking in late winter and early spring <br><br> Harvest when the lemons are yellow or almost yellow and have a shiny appearance.";
+
+var kumquat = "<strong>Kumquats / Citrus Japonica </strong> <br><br> Origin: China <br> Blooming season: Mid-summer <br><br> Harvest when fruit is bright orange.";
+
+var oro = "<strong>Oro Blanco Grapefruit / Citrus Maxima × Citrus Paradisi</strong> <br><br> Origin: Created at UC Riverside in 1958 <br> Hybrid of pummelo and grapefruit <br><br> Harvest during winter and the beginning of spring when at least half of the peel has turned yellow.";
+
+var grape = "<strong>Table Grapes / Vitis Vinifera </strong> <br><br> Origin: Descended from varieties in Iran and Turkey <br><br> Harvest when the fruits are plump and colorful in early summer.";
+
+var strawberry = "<strong>Strawberries / Fragaria × Ananassa</strong> <br><br> Origin: Brittany, France <br><br> Harvest from early winter to late summer when the berries are bright red.";
+
+var loquat = "<strong>Loquats / Eriobotrya Japonica</strong> <br><br> Origin: China <br><br> Harvest during the summer when the fruit is a golden yellow color.";
+
+var nopales = "<strong>Nopales / Opuntia</strong> <br><br> Origin: Mexico <br> Blooming season: Spring <br><br> Harvest the pads of the cactus at any time of year; to avoid acidic pads, harvest them mid-morning. The fruits may be harvested when they are dark red or purple before they bloom.";
+
+var pomegranate = "<strong>Pomegranates / Punica Granatum</strong> <br><br> Origin: Throughout the Middle East and the Mediterranean <br><br> Harvest September through February when the fruit makes a metallic sound when it is tapped.";
+
+var sapotes = "<strong>White Sapotes / Casimiroa Edulis</strong> <br><br> Origin: Central Mexico <br><br> Harvest the fruits when they are bright green or orange-yellow, depending on the type, between October and February. Cut the fruit off of the plant leaving a little bit of stem attached; it will fall off when the fruit is ready to eat.";
+
+var mulberry = "<strong>Black Persian Mulberry / Morus Nigra</strong> <br><br> Origin: Southwest Asia <br><br> Harvest during the summer when the fruits appear juicy and fat.";
+
+var meyer = "<strong>Meyer Lemon / Citrus × Meyeri</strong> <br><br> Origin: China <br><br> Harvestable year-round when the fruit is bright yellow or nearly yellow.";
+
+var laurel = "<strong>Bay Laurel / Laurus Nobilis</strong> <br><br> Origin: Mediterranean <br><br> Bay laurel leaves are harvestable year-round once the tree is several years old.";
+
+var oak = "<strong>California Live Oak / Quercus Agrifolia</strong> <br><br> Origin: California and Mexico <br> Blooming season: Winter and spring <br><br> The acorns are harvestable seven to eight months after pollination.";
+
+var avocado = "<strong>Avocados / Persea Americana</strong> <br><br> Origin: South and Central America <br> Blooming season: Late summer through winter <br><br> Harvest generally occurs year-round. Once the fruit is picked from the tree, it begins to ripen.";
+
+var olive = "<strong>Olives / Olea Europaea</strong> <br><br> Origin: Mediterranean <br> Blooming season: May <br><br> Harvest during September through mid-November when the fruit is beginning to darken.";
+
+var hPersimmon = "<strong>Hachiya persimmon / Diospyros kaki “Hachiya”</strong> <br><br> Origin: China <br><br> Harvest when the fruit is hard but a bright red-orange color, typically in November through December. The fruit is often used in baked goods.";
+
+var rosemary = "<strong>Rosemary / Rosmarinus Officinalis</strong> <br><br> Origin: Mediterranean <br><br> Leaves are harvestable year-round as long as no more than six inches are snipped off each stem at a time.";
+
+var veggie = "<strong>Veggies and Herbs</strong> <br><br> Origin: Variable <br><br> Harvest depends on the type of herb or vegetable. Ask a member of the gardening club or do some research online!";
+
+var jujuba = "<strong>Jujube / Ziziphus Jujuba</strong> <br><br> Origin: East Asia <br> Blooming season: April and May <br><br> Harvest when the seeds become ripe starting in October. The leaves of this plant may also be consumed.";
+
+var kiwi = "<strong>Kiwi / Actinidia Deliciosa</strong> <br><br> Origin: China <br><br> Harvest in October and the start of September.";
+
+var carob = "<strong>Carob Tree / Ceratonia Siliqua</strong> <br><br> Origin: Mediterranean <br> Blooming season: Late fall through early winter <br><br> Harvest the pods in the spring while they are green and flexible.";
+
+var banana = "<strong>Banana / Musa</strong> <br><br> Origin: Latin America <br> Blooming season: June or July <br><br>The fruit is harvestable year-round and becomes ripe nine months after pollination. Bananas are still green when they should be picked.";
+
+var fPersimmon = "<strong>Fuyu Persimmon / Diospyros kaki “Fuyu”</strong> <br><br> Origin: China <br><br> Harvest when the fruit is a soft orange color between October and December.";
+
+var apple = "<strong>Apple / Malus</strong> <br><br> Origin: Asia <br> Blooming season: Spring <br><br> Harvest in the summer through the fall; it’s easiest to tell if an apple is ripe or not by trying one.";
+
+var valentine = "<strong>Valentine Grapefruit / Citrus maxima “Valentine”</strong> <br><br> Origin: Created at UC Riverside in the 1950s <br><br> Harvest the fruit between January and March in time for Valentine’s Day";
+
+var blueberry = "<strong>Blueberries / Cyanococcus</strong> <br><br> Origin: United States of America <br><br> Harvest in May and June when the berries are large, bright blue, and easy to pull off of the bush.";
+
+var minneola = "<strong>Minneola Tangelos / Citrus × Tangelo</strong> <br><br> Origin: Created in labs in Eustis, Florida and Riverside, California in the 1890s <br><br> Harvest January through April when the fruit is bright orange and feels hard and heavy for its size.";
+
+var fig = "<strong>Figs / Ficus Carica</strong> <br><br> Origin: Asia Minor <br><br> Harvests occur twice a year; times may vary, but they typically occur between June and September.";
+
+var lime = "<strong>Mexican Key Limes / Citrus Aurantifolia</strong> <br><br> Origin: Southeast Asia <br><br> Harvest between October and December when the fruit is bright green and smells sweet.";
+
+
+function writeText(txt)
+{
+    document.getElementById("desc").innerHTML = txt;
+}
---
+++ indexes.js
@@ -0,0 +1,69 @@
+var seville = "<strong>Naranja amarga / Citrus Aurantium</strong> <br><br> Origen:  El sureste de Asia <br> Temporada de floración: Invierno y verano <br><br> Cosecha cuando la fruta es naranja brillante y algunas de las naranjas están empezando a caer del árbol.";
+
+var navel = "<strong>Naranja nável / Citrus Sinensis “Navel”</strong> <br><br> Origen: Brasil <br> Temporada de floración: Invierno y primavera <br><br> Cosecha durante el invierno y la primavera cuando la fruta es naranja brillante y algunas de las naranjas están empezando a caer del árbol.";
+
+var valencia = "<strong>Naranja valencia / Citrus Sinensis “Valencia”</strong> <br><br> Origen: Santa Ana, California <br> Híbrido del pomelo y la naranja mandarín <br> Temporada de floración: Invierno y primavera <br><br> Cosecha durante el verano y el invierno cuando la fruta es naranja brillante y algunas de las naranjas están empezando a caer del árbol.";
+
+var eureka = "<strong>Limón eureka / Citrus Limon</strong> <br><br> Origen: China <br> Temporada de floración: Todo el año, alcanzando su punto máximo al final del invierno y el principio de la primavera <br><br> Cosecha cuando los limones son amarillos o casi amarillos y tienen una apariencia brillante.";
+
+var kumquat = "<strong>Kumquat / Citrus Japonica </strong> <br><br> Origen: China <br> Temporada de floración: Mediados del verano <br><br> Cosecha cuando la fruta es naranja brillante.";
+
+var oro = "<strong>Toronja de oro blanco / Citrus Maxima × Citrus Paradisi</strong> <br><br> Origen: Creado en UC Riverside en 1958 <br> Híbrido del pomelo y toronja <br><br> Cosecha durante el invierno y el principio de la primavera cuando al menos la mitad de la cáscara es amarilla.";
+
+var grape = "<strong>Uva de mesa / Vitis Vinifera </strong> <br><br> Origen: Tienen descendencia de variedades en Irán y Turquía <br><br> Cosecha cuando las frutas son gordas y llenas de color en el principio del verano.";
+
+var strawberry = "<strong>Fresa / Fragaria × Ananassa</strong> <br><br> Origen: Brittany, Francia <br><br> Cosecha desde el principio del invierno al final del verano cuando las moras son rojas brillantes.";
+
+var loquat = "<strong>Loquat / Eriobotrya Japonica</strong> <br><br> Origen: China <br><br> Cosecha durante el verano cuando la fruta es un color dorado.";
+
+var nopales = "<strong>Nopales / Opuntia</strong> <br><br> Origen: México <br> Temporada de floración: Primavera <br><br> Cosecha las hojas de los nopales en cualquier época del año; para evitar hojas ácidas, cosecha a media mañana. Las frutas se pueden ser cosechadas cuando son rojas oscuras o moradas antes de floración.";
+
+var pomegranate = "<strong>Granada / Punica Granatum</strong> <br><br> Origen: En todo el Oriente Medio y el Mediterráneo <br><br> Cosecha de septiembre a febrero cuando la fruta hace un sonido metálico cuando es tocada.";
+
+var sapotes = "<strong>Zapote blanco / Casimiroa Edulis</strong> <br><br> Origen: México central <br><br> Cosecha las frutas cuando son verdes brillantes o naranja-amarillas, dependiendo del tipo, entre octubre y febrero. Corta la fruta de la planta dejando un poco del tallo sujetado; caerá cuando la fruta esté lista para comer.";
+
+var mulberry = "<strong>Morera persa negra / Morus Nigra</strong> <br><br> Origen: El suroeste de Asia <br><br> Cosecha durante el verano cuando las frutas aparecen jugosas y gordas.";
+
+var meyer = "<strong>Limón meyer/ Citrus × Meyeri</strong> <br><br> Origen: China <br><br> Cosechado todo el año cuando la fruta es amarilla brillante o casi amarilla.";
+
+var laurel = "<strong>Laurel de la bahía / Laurus Nobilis</strong> <br><br> Origen: El Mediterráneo <br><br> Las hojas del laurel de la bahía son cosechadas durante todo el año cuando el árbol tiene varios años.";
+
+var oak = "<strong>Roble vivo de California / Quercus Agrifolia</strong> <br><br> Origen: California y México <br> Temporada de floración: Invierno y primavera <br><br> Las bellotas son cosechadas siete a ocho meses después de la polinización.";
+
+var avocado = "<strong>Aguacate / Persea Americana</strong> <br><br> Origen: América del Sur y América Central <br> Temporada de floración: Del final del verano al invierno <br><br> La cosecha ocurre durante todo el año por lo general. Cuando la fruta es recogida del árbol, empieza a madurar.";
+
+var olive = "<strong>Olivo / Olea Europaea</strong> <br><br> Origen: El Mediterráneo <br> Temporada de floración: Mayo<br><br> Cosecha de septiembre al medio de noviembre cuando la fruta empieza a oscurecer.";
+
+var hPersimmon = "<strong>Caqui de hachiya / Diospyros kaki “Hachiya”</strong> <br><br> Origen: China <br><br> Cosecha cuando la fruta es dura pero tiene un color roja-naranja brillante, típicamente de noviembre a diciembre. A menudo la fruta es usada en productos horneados.";
+
+var rosemary = "<strong>Romero / Rosmarinus Officinalis</strong> <br><br> Origen: El Mediterráneo <br><br> Las hojas son cosechadas durante todo el año con tal de que no cortes más de seis pulgadas de cada tallo a la vez.";
+
+var veggie = "<strong>Verduras y hierbas</strong> <br><br> Origen: Variable <br><br> La cosecha depende del tipo de hierba o verdura. ¡Pregunta unx miembrx del club de la jardinería o haz un poco de investigación en línea!";
+
+var jujuba = "<strong>Gominola / Ziziphus Jujuba</strong> <br><br> Origen: El este de Asia <br> Temporada de floración: Abril y mayo <br><br> Cosecha cuando las semillas maduran empezando en octubre. También se puede consumir las hojas de esta planta.";
+
+var kiwi = "<strong>Kiwi / Actinidia Deliciosa</strong> <br><br> Origen: China <br><br> Cosecha durante octubre y el principio de septiembre.";
+
+var carob = "<strong>Algarrobo / Ceratonia Siliqua</strong> <br><br> Origen: El Mediterráneo <br> Temporada de floración: A fines del otoño y el principio del invierno <br><br> Cosecha las vainas en la primavera cuando son verdes y flexibles.";
+
+var banana = "<strong>Plátano / Musa</strong> <br><br> Origen: Latinoamérica <br> Temporada de floración: Junio o julio <br><br> La fruta es cosechada durante todo el año y se hace madura nueve meses después de la polinización. Los plátanos deben ser verdes cuando son recogidos del árbol.";
+
+var fPersimmon = "<strong>Caqui de fuyu / Diospyros kaki “Fuyu”</strong> <br><br> Origen: China <br><br> Cosecha cuando la fruta es un color naranja suave entre octubre y diciembre.";
+
+var apple = "<strong>Manzana / Malus</strong> <br><br> Origen: Asia <br> Temporada de floración: Primavera <br><br> Cosecha del verano al otoño; para saber si un manzana es madura, pruébala.";
+
+var valentine = "<strong>Toronja de Valentín / Citrus maxima “Valentine”</strong> <br><br> Origen: Creado en UC Riverside durante los 1950s <br><br> Cosecha la fruta entre enero y marzo justo a tiempo para el Día de San Valentín.";
+
+var blueberry = "<strong>Arándano / Cyanococcus</strong> <br><br> Origen: Los Estados Unidos <br><br> Cosecha en mayo y junio cuando las moras son grandes y azules brillantes y es fácil recolectarlas del arbusto.";
+
+var minneola = "<strong>Minneola Tangelo / Citrus × Tangelo</strong> <br><br> Origen: Creado en laboratorios en Eustis, Florida y Riverside, California en los 1890s <br><br> Cosecha entre enero y abril cuando la fruta es naranja brillante y parece dura y pesada por su tamaño.";
+
+var fig = "<strong>Higo / Ficus Carica</strong> <br><br> Origen: Asia Menor <br><br> La cosecha ocurre dos veces cada año; los tiempos pueden variar pero típicamente ocurren entre junio y septiembre.";
+
+var lime = "<strong>Higo / Ficus Carica</strong> <br><br> Origen: Asia Menor <br><br> La cosecha ocurre dos veces cada año; los tiempos pueden variar pero típicamente ocurren entre junio y septiembre.";
+
+
+function writeText(txt)
+{
+    document.getElementById("desc").innerHTML = txt;
+}
